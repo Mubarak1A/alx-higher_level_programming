91b342a1a6c4b1400f0259c6d2b9e0a747c354f6
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-01-11 22:58:52 +0100

    Sorted key dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = [key for key in (a_dictionary.keys())].sort()
+    sorted_keys = [key for key in (a_dictionary.keys())]
+    sorted_keys.sort()
     new_dict = {key: a_dictionary[key] for key in sorted_keys}
 
-    print(new_dict)
+    for key in new_dict.keys()
+        print("{}: {}".format(key, new_dict[key])
