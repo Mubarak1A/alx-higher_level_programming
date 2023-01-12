0c3aacb16f413f658b4a0089fdbb77efae7bbdf7
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-01-12 10:11:21 +0000

    val * 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_d = {}
-    for i in a_dictionary:
-        new_d[i] = a_dictionary[i] * 2
-    return new_d
+    for i in a_dictionary.values():
+        i *= 2
+    return a_dictionary
