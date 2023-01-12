b575195074bae319c37505a406c3c688f0ce33e4
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-01-12 09:55:05 +0000

    pycode val

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = [key for key in (a_dictionary.keys())]
     sorted_keys.sort()
-    
     for key in sorted_keys:
         print("{}: {}".format(key, a_dictionary[key]))
