7223d716b2c2806082231fd5ef3eb0364fe4997a
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-01-12 10:38:08 +0000

    max value key

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        for i in a_dictionary.key():
+        for i in a_dictionary.keys():
             if a_dictionary[i] == max(a_dictionary.values()):
                 return i
