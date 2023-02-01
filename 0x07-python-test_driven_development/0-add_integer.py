390b0a64c4fdcdf64d785b047ce4c8b31cc904a4
Mubarak1A <adesinamubarak123@example.com>
2023-02-01 14:25:44 +0000

    doctestr

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module for addition function
+
+    Function: add_integer
+
+    return: addition of two integer argument
+"""
+def add_integer(a, b=98):
+    """Compute and return the sum of two integer/float arguments.
+    
+    Examples:
+        >>>add_integer(4.0, 2)
+        6
+        >>>add_integer(4, -2)
+        2
+        >>>add_integer(4.0, -2.0)
+        2
+        >>>add_integer(2)
+        100
+
+    Args:
+        a (int/float): A number representing the first arg
+        b (int/float): A number representing the secound arg with default value 98.
+    returns:
+        int: Sum of the two args 
+    """
+    if(type(a) != int and type(a) != float):
+        raise TypeError("a must be an integer")
+    if(type(b) != int and type(b) != float):
+        raise TypeError("b must be an integer")
+    
+    a = int(a)
+    b = int(b)
+
+    return a + b
+
+    #if __name__ == "__main__":
+     #   import doctest
+      #  doctest.testmod()
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,6 @@
+# 0x07-python-test_driven_development
+
+test  -  Folder for test folder
+
+0-add_integer.py  -  module for  function that adds 2 integers.
+
