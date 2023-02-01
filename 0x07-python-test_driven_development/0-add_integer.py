37f90d714df2933c429512fee0b987dbc1649827
Mubarak1A <adesinamubarak123@example.com>
2023-02-01 14:37:55 +0000

    doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -24,9 +24,9 @@ def add_integer(a, b=98):
     returns:
         int: Sum of the two args 
     """
-    if(type(a) != int and type(a) != float):
+    if not isinstance(a, int, float):
         raise TypeError("a must be an integer")
-    if(type(b) != int and type(b) != float):
+    if not isinstance(b, int, float):
         raise TypeError("b must be an integer")
     
     a = int(a)
