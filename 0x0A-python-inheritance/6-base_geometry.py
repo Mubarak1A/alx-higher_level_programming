3bddf9fb912d1158efd10cc44250f2a010607837
Mubarak1A <adesinamubarak123@example.com>
2023-02-09 00:31:15 +0100

    no attr

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Module for a class BaseGeometry
+    with a Public instance method: def area(self):
+"""
+
+class BaseGeometry():
+    """Class with no attributes"""
+
+    def area(self):
+        """
+             raises an Exception with the message area() is not implemented (as their is no attribute)
+        """
+        raise("area() is not implemented")
