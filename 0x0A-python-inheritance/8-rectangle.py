5a29653ad76dfb7a840392ffdeaa45dad7d11499
Mubarak1A <adesinamubarak123@example.com>
2023-02-09 00:56:05 +0100

    inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+    Module of a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class inherit from BaseGeometry"""
+    
+    def __init__(self, width, height):
+        """Class attributes"""
+        self.width = width
+        self.height = height
