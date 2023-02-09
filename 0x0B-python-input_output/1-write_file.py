64352ebb5cea3df12b8a45d36f9f3b157f3d8e25
Mubarak1A <adesinamubarak123@example.com>
2023-02-09 23:43:54 +0100

    write to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""""Module for function write_file"""
+
+def write_file(filename="", text=""):
+    """
+        function that writes to a text file (UTF8) and returns the number of characters written:
+
+        Args:
+            filename (txt file)
+            text (sting)
+
+        return:
+            int (number of characters written)
+    """
+    with open("filename", mode="w", encoding="UTF-8") as myFile:
+        myFile.write(text)
+    
+    char = 0
+    for word in text:
+        for char in word:
+            char += 1
+    
+    return char
+    
+    
