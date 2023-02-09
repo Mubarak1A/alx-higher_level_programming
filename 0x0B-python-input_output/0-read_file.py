947d26f975a7f6190108a9f2f93524199b266a84
Mubarak1A <adesinamubarak123@example.com>
2023-02-09 23:26:38 +0100

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""Module for function read_file"""
+
+def read_file(filename=""):
+    """
+        function that reads a text file (UTF8) and prints it to stdout:
+
+        Args:
+            filename (txt file)
+    """
+    with open("filename", encoding="UTF-8") as myFile:
+        print(myFile.read())
