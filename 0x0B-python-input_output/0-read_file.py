c0e0ba73c98af03dfe824b8863833a21778b66e4
Mubarak1A <adesinamubarak123@example.com>
2023-02-10 00:30:27 +0100

    read line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """"Module for function read_file"""
 
+
 def read_file(filename=""):
-    """
-        function that reads a text file (UTF8) and prints it to stdout:
+    """function that reads a text file (UTF8) and prints it to stdout:
 
         Args:
             filename (txt file)
