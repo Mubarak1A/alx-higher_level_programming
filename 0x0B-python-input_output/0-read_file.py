cfc7c597372ac77f3ef9e20910aa21fadf1e87f9
Mubarak1A <adesinamubarak123@example.com>
2023-02-11 10:11:23 +0100

    just trying

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """"Module for function read_file"""
 
+
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout:
 
         Args:
             filename (txt file)
     """
-    with open("filename", encoding="utf-8") as myFile:
+    with open(filename, encoding="utf-8") as myFile:
         print(myFile.read())
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """"Module for function write_file"""
 
+
 def write_file(filename="", text=""):
     """
-        function that writes to a text file (UTF8) and returns the number of characters written:
+        function that writes to a text file (UTF8) and returns the number of
+        characters written:
 
         Args:
             filename (txt file)
@@ -12,17 +14,15 @@ def write_file(filename="", text=""):
         return:
             int (number of characters written)
     """
-    with open("filename", mode="w", encoding="utf-8") as myFile:
+    with open(filename, mode="w", encoding="utf-8") as myFile:
         myFile.write(text)
 
     with open("filename", encoding="utf-8") as myFile:
         my_file = myFile.read()
-    
+        
     char = 0
     for word in my_file:
         for char in word:
             char += 1
-    
+               
     return char
-
-    
