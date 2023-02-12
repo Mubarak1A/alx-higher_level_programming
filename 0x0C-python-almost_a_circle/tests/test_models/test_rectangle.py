469fbc47dc46563a5874f5e9420dd320aa3c147b
Mubarak1A <adesinamubarak123@example.com>
2023-02-12 23:11:06 +0100

    exception raise

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,6 +14,8 @@ class TestBase(unittest.TestCase):
         r1 = Rectangle(10, 2)
         r2 = Rectangle(2, 10)
         r3 = Rectangle(10, 2, 0, 0, 12)
+        r4 = Rectangle(10, "2", 0.0, 0.0, 12.0)
+        r5 = Rectangle(10, 0, -3, -8, -12)
 
         self.assertEqual(r1.id, 1)
         self.assertEqual(r2.id, 2)
@@ -21,6 +23,18 @@ class TestBase(unittest.TestCase):
         self.assertEqual(r3.x, 0)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r2.height, 10)
+
+        with self.assertRaises(TypeError):
+            r4.width
+            r4.height
+            r4.x
+            r4.y
+
+        with self.assertRaises(ValueError):
+            r5.width
+            r5.height
+            r5.x
+            r5.y
 
 
 if __name__ == "__main__":
