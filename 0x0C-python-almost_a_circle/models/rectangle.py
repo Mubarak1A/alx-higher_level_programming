aace4308735fb19ebe1f5407dd5e8eb37e165629
Mubarak1A <adesinamubarak123@example.com>
2023-02-12 23:45:13 +0100

    arean method and test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,7 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        """return the area of rectangle"""
+        return self.__width * self.__heigth
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,14 +8,17 @@ from models.rectangle import Rectangle
 class TestBase(unittest.TestCase):
     """Test class for Base class"""
 
-    def test_rectangle(self):
-        """test method for Rectangle class"""
+    def setUp(self):
+        """General test variable"""
 
         r1 = Rectangle(10, 2)
         r2 = Rectangle(2, 10)
-        r3 = Rectangle(10, 2, 0, 0, 12)
+        r3 = Rectangle(10, 2, 8, 0, 12)
         r4 = Rectangle(10, "2", 0.0, 0.0, 12.0)
         r5 = Rectangle(10, 0, -3, -8, -12)
+
+    def test_rectangle(self):
+        """test method for Rectangle class"""
 
         self.assertEqual(r1.id, 1)
         self.assertEqual(r2.id, 2)
@@ -35,6 +38,18 @@ class TestBase(unittest.TestCase):
             r5.height
             r5.x
             r5.y
+
+    def test_area(self):
+        """test for area method"""
+
+        self.assertEqual(r1.area(), 20)
+        self.assertEqual(r3.area(), 16)
+
+        with self.assertRaises(TypeError):
+            r4.area()
+
+        with self.assertEqual(ValueError):
+            r5.area()
 
 
 if __name__ == "__main__":
