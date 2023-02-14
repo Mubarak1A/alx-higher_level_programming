6e07f1bdd4dadbf51031fdfa4b208e693ae1274c
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-02-14 11:15:33 +0100

    Confused

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,8 +9,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(id, x, y, width, height)
-        self.width = size
-        self.height = size
 
     @property
     def size(self):
